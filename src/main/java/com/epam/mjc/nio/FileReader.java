3f675e2b10ca1e964293446644af92e81ae6bf6a
Petya4000 <pt.bartosh54321@gmail.com>
2022-10-22 10:48:55 +0300

    com1

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,54 +1,19 @@
 package com.epam.mjc.nio;
 
 import java.io.*;
-import java.nio.ByteBuffer;
-import java.nio.channels.FileChannel;
-
 
 public class FileReader {
-
     public Profile getDataFromFile(File file) {
-        ByteBuffer buffer = null;
-        try (RandomAccessFile raf = new RandomAccessFile(file, "r");
-             FileChannel channel = raf.getChannel()) {
-
-            buffer = ByteBuffer.allocate(400);
-            StringBuilder stringBuilder = new StringBuilder();
-            while ((channel.read(buffer)) > 0) {
-                buffer.flip();
-                for (int i = 0; i < buffer.limit(); i++) {
-                    stringBuilder.append((char) buffer.get());
-                }
-            }
-
-            String str = stringBuilder.toString();
-            int start = str.indexOf(':');
-            int end = str.indexOf('\n');
-            String name = str.substring(start + 2, end - 1);
-            str = str.substring(end + 1);
-
-            start = str.indexOf(':');
-            end = str.indexOf('\n');
-            String age = str.substring(start + 2, end - 1);
-            Integer intAge = age.isEmpty() ? null : Integer.parseInt(age);
-            str = str.substring(end + 1);
-
-            start = str.indexOf(':');
-            end = str.indexOf('\n');
-            String email = str.substring(start + 2, end - 1);
-            str = str.substring(end + 1);
-
-            start = str.indexOf(':');
-            end = str.indexOf('\n');
-            String phone = str.substring(start + 2, end - 1);
-            Long longPhone;
-            if (phone.isEmpty() || phone.equals(" "))
-                longPhone = 0L;
-            else
-                longPhone = Long.parseLong(phone);
-
-            return new Profile(name, intAge, email, longPhone);
-
+        String name;
+        int age;
+        String email;
+        long phone;
+        try (BufferedReader inputStream = new BufferedReader(new java.io.FileReader(file))) {
+            name = inputStream.readLine().substring(6);
+            age = Integer.parseInt(inputStream.readLine().substring(5));
+            email = inputStream.readLine().substring(7);
+            phone = Long.parseLong(inputStream.readLine().substring(7));
+            return new Profile(name, age, email, phone);
         } catch (IOException e) {
             e.printStackTrace();
             return null;
